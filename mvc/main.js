f1e14cbef7c5af30e008d21e4824487355df3387
dudueasy <dudueasy@126.com>
2018-06-24 01:16:58 +0800

    使用构造函数来重写MVC结构

--- mvc/main.js
+++ mvc/main.js
@@ -1,26 +1,68 @@
+// Globally activate axios.interceptors
 fakeData()
 
-// controller.init({view,model})
+// define Model
+function Model(options) {
+  this.data = options.data
+  this.resource = options.resource
 
-let model = {
-  data: {
-    name: '',
-    number: 0,
-    id: ''
-  },
-  fetch(id) {
-    return axios.get(`/books/${id}`).then((response) => {
-      this.data = response.data
-      return response
-    })
-  },
-  update(id, data) {
-    return axios.put(`/books/${id}`, data)
-  }
 }
 
-// define view
-let view = {
+
+Model.prototype.fetch = function(id) {
+  return axios.get(`/${this.resource}/${id}`).then((response) => {
+
+    this.data = response.data
+    return response
+  })
+}
+
+
+Model.prototype.update = function(data) {
+  let id = this.data.id
+  console.log('update:', data)
+  console.log(`${this.resource}`, `${id}`)
+
+  return axios.put(`/${this.resource}/${id}`, data)
+}
+
+
+// make a instance of Model
+let model = new Model({
+  data: {
+    name: 'xx',
+    number: 0,
+    id: '',
+  },
+  resource: 'books'
+
+})
+
+window.model = model
+
+
+
+
+//define View
+function View({
+  el, template
+}) {
+  this.el = el
+  this.template = template
+}
+
+// 定义模板渲染机制
+View.prototype.render = function(data) {
+  let html = this.template
+  for (let key in data) {
+    html = html.replace(`__${key}__`, data[key])
+  }
+
+  $(this.el).html(html)
+}
+
+// Instanciate view
+let view = new View({
   el: '#app',
   template: `
   <div>
@@ -33,19 +75,12 @@ let view = {
     <button id='minusOne'>减一</button>
     <button id='reset'>归零</button>
   </div>
-`,
-  render(data) {
-    let html = this.template.replace('__name__', data.name).replace('__number__', data.number)
-    $(this.el).html(html)
-  }
+`
+})
 
-}
+// define controller
+// use init() to bind view, model to controller, and render html for initiation 
 
-console.log(1)
-  // define controller
-  // use init() to bind view, model to controller, and render html for initiation 
-
-console.log(2)
 var controller = {
   init(options) {
       let {
@@ -55,29 +90,43 @@ var controller = {
       // 初次渲染 view 节点
       this.view = view
       this.model = model
+
       this.view.render(this.model.data)
       this.bindEvents()
 
       // 渲染请求第一本书获得的数据.
       this.model.fetch(1).then(({}) => {
         this.view.render(this.model.data)
+
       })
     },
     addOne() {
+      console.log('addOne()')
 
       let oldNumber = $('#count').text() - 0
       let newNumber = oldNumber + 1
-      this.model.update({number:newNumber}).then(()=>{
+
+
+      // bug start here================================
+      this.model.update({
+        number: newNumber
+      }).then(() => {
+
         this.view.render(this.model.data)
-        })
-     },
+
+        // bug end here===================================
+        console.log("this.model.data:", this.model.data)
+      })
+    },
     minusOne() {
       let oldNumber = $('#count').text() - 0
       let newNumber = oldNumber - 1
 
-      this.model.update({number:newNumber}).then(()=>{
+      this.model.update({
+        number: newNumber
+      }).then(() => {
         this.view.render(this.model.data)
-        })
+      })
 
 
     },
@@ -85,17 +134,17 @@ var controller = {
 
       let newNumber = 0
 
-      this.model.update({number:newNumber}).then(()=>{
+      this.model.update({
+        number: newNumber
+      }).then(() => {
         this.view.render(this.model.data)
-        })
-
-
+      })
     },
     bindEvents() {
 
       $(this.view.el).on('click', '#addOne', this.addOne.bind(this))
 
-      $(this.view.el).on('click', '#minusOne',this.minusOne.bind(this))
+      $(this.view.el).on('click', '#minusOne', this.minusOne.bind(this))
 
       $(this.view.el).on('click', '#reset', this.reset.bind(this))
     }
@@ -104,46 +153,13 @@ var controller = {
 
 
 
-// click event handler
-$('#app').on('click', '#addOne', () => {
-  let oldNumber = $('#count').text() - 0
-  let newNumber = oldNumber + 1
-
-  axios.put('/books/1', {
-    number: newNumber
-  }).then(() => {
-    $('#count').text(newNumber)
-  })
-})
-
-$('#app').on('click', '#minusOne', () => {
-  let oldNumber = $('#count').text() - 0
-  let newNumber = oldNumber - 1
-
-  axios.put('/books/1', {
-    number: newNumber
-  }).then(() => {
-    $('#count').text(newNumber)
-  })
-})
-
-$('#app').on('click', '#reset', () => {
-  let newNumber = 0
-  axios.put('/books/1', {
-    number: newNumber
-  }).then(() => {
-    $('#count').text(0)
-  })
-})
-
-
 // Initiate variable book. Globally activate axios.interceptors
 function fakeData() {
 
   let book = {
     name: 'JavaScript 高级程序设计',
     number: 2,
-    id: 1
+    id: 1,
   }
 
   //axios拦截器, 拦截并处理 response
@@ -153,15 +169,16 @@ function fakeData() {
       url, method, data
     } = response.config
 
-    if (url == "/books/1" && method == "get") {
+    if (url === "/books/1" && method == "get") {
       response.data = book
-    } else if (url == "/books/1" && method == "put") {
+    } else if (url === "/books/1" && method == "put") {
+
+      console.log('put data:', data)
       data = JSON.parse(data)
       Object.assign(book, data)
       response.data = book
     }
 
-    console.log('response.config: ', response.config)
     return response
   })
 }
